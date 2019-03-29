796d9db3488741771e16696f52ab492eb4c5d243
dboydgit <derrykboyd@gmail.com>
2019-03-28 23:41:10 -0600

    User stories 1-3

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 'use strict';
 
+require('dotenv').config();
+
 var express     = require('express');
 var bodyParser  = require('body-parser');
 var expect      = require('chai').expect;
@@ -8,8 +10,14 @@ var cors        = require('cors');
 var apiRoutes         = require('./routes/api.js');
 var fccTestingRoutes  = require('./routes/fcctesting.js');
 var runner            = require('./test-runner');
+const helmet = require('helmet');
 
 var app = express();
+
+app.use(helmet({
+  frameguard: {action: 'sameorigin'},
+  referrerPolicy: {policy: 'same-origin'}
+}));
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
